9122b1d5ea0cf234b976132dd04aafaf05990a0d
benwagley <benwagley@25.81.96.25>
2016-06-14 15:16:55 -0400

    Fixed Graph .

--- Thrust.cpp
+++ Thrust.cpp
@@ -8,9 +8,9 @@
 
 //Calculates how far a projectile will travel without thrust
 
-double * coast(Projectile r, World b, double V /*Velocity*/,
-        double Vt /*Direction in Degrees */) {
+double * coast(Projectile r, World b, double Vx /*Velocity On X*/, double Vy /*Velocity on Y*/) {
     FILE *data = fopen("data.dat", "a");
+
     static double outCoast[3];
     double m = r.getMass();
     double cx = r.getDragX();
@@ -19,14 +19,12 @@ double * coast(Projectile r, World b, double V /*Velocity*/,
     double Ay = r.getAreaY();
     double d = b.getDensity();
     double g = b.getGravity();
-    double Vx = V * cos(Vt * PI / 180);
-    double Vy = V * sin(Vt * PI / 180);
-    double Fdx, Fdy, ax, ay, Vx2, Vy2, Dx, Dy;
+    double Vt, Fdx, Fdy, ax, ay, Vx2, Vy2, Dx, Dy;
     double t = 0;
     double tstep = .01;
 
     while (r.getDistY() >= 0) {
-        Vt = atan(Vy / Vx) * 180 / PI;
+        Vt = atan(Vy / Vx) * (180 / PI);
         Dx = (cx * d * Ax * Vx * Vx) / 2;
         Dy = (cy * d * Ay * Vy * Vy) / 2;
         ax = -Dx / m;
@@ -59,6 +57,7 @@ double * coast(Projectile r, World b, double V /*Velocity*/,
 double * thrust(Rocket r, World b, double lt) {
     FILE *data = fopen("data.dat", "w");
     
+    lt = lt * PI / 180;
     int pointCount = 0;
     double outThrust[3];
     double *outC;
@@ -70,8 +69,8 @@ double * thrust(Rocket r, World b, double lt) {
     double d = b.getDensity();
     double g = b.getGravity();
     double T = r.getThrust();
-    double Tx = T * cos(lt * PI / 180);
-    double Ty = T * sin(lt * PI / 180);
+    double Ty = T * sin(lt);
+    double Tx = T * cos(lt);
     double Vx = 0;
     double Vy = 0;
     double Fdx, Fdy, ax, ay, Vx2, Vy2, Dx, Dy;
@@ -81,17 +80,17 @@ double * thrust(Rocket r, World b, double lt) {
     double tstep = .01;
     while (t <= r.getBurn()) {
         Vx = Vx + ((Tx / m) * tstep);
-        Vy = Vy + ((Ty + 9.8) / m) * tstep;
+        Vy = Vy + (((Ty + 9.8) / m) * tstep);
         Dx = (cx * d * Ax * Vx * Vx) / 2;
         Dy = (cy * d * Ay * Vy * Vy) / 2;
         Vx2 = Vx - (Dx / m) * tstep;
         Vy2 = Vy - (Dx / m) * tstep;
         r.setDistX(r.getDistX() + (Vx2 * tstep));
-        r.setDistY(r.getDistY() + (Vx2 * tstep));
+        r.setDistY(r.getDistY() + (Vy2 * tstep));
         Vx = Vx2;
         Vy = Vy2;
         t = t + tstep;
-
+ 
         r.setMass(m + r.getFlow() / tstep);
         fprintf(data, "%f %f\n", r.getDistX(), r.getDistY());
 
@@ -100,7 +99,7 @@ double * thrust(Rocket r, World b, double lt) {
     }
     fflush(data);
     fclose(data);
-    outC = coast(r, b, sqrt((Vx * Vx) + (Vy * Vy)), atan((Vy / Vx)) * 180 / PI);
+    outC = coast(r, b, Vx, Vy);
     outThrust[0] = r.getDistX();
     outThrust[1] = r.getDistY();
     outThrust[2] = t + outC[2];
